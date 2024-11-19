6db10969744ba85a4587fe9d9fbf4dcb4acbe6cd
angie-kinya <angiekinya670@gmail.com>
2024-11-19 12:58:31 +0300

    Personal Portfolio Website
    description: Portfolio with about me, link to resume, projects, skills, and contact details.

--- script.js
+++ script.js
@@ -1,19 +1,59 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Smooth scrolling for navigation links
-    const navLinks = document.querySelectorAll('nav ul li a');
+function sendMail(params){
+    var tempParams = {
+        from_name: document.getElementById("fromName").value,
+        email_sender: document.getElementById("emailSender").value,
+        subject_sender: document.getElementById("subjectSender").value,
+        message_sender: document.getElementById("message").value,
+    };
+    emailjs.send('service_8c1iuyk','template_b6cpa9u',tempParams)
+     .then(function(res){
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Mail Sent Successfully!',
+            showConfirmButton: false,
+            timer: 1500,
+          });
+     });
+}
 
-    for (const link of navLinks) {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            const sectionID = event.target.getAttribute('href').substring(1);
-            const section = document.getElementById(sectionID);
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $(".navbar").addClass("sticky");
+        } else {
+            $(".navbar").removeClass("sticky");
+        }
+    });
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
 
-            if (section) {
-                window.scrollTo({
-                    top: section.offsetTop,
-                    behavior: 'smooth'
-                });
-            }
-        });
-    }
+    // typing animation script
+    var typed = new Typed(".typing",{
+        strings: ["Data Analyst","Web Developer","Software Developer","Data Scientist"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+        fadeOut: true,
+        
+    });
+    var typed = new Typed(".typing-2",{
+        strings: ["Data Analyst","Web Developer","Software Developer","Data Scientist"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    });
 });
+
+     // Scroll to top button script
+      const btnScrollToTop = document.querySelector("#btnScrollToTop");
+
+      btnScrollToTop.addEventListener("click", function(){
+        window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+      });
+    });
